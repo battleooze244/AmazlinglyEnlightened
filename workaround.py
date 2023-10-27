13ef049e3573b0a1732136a872820741f1aeff21
battleooze244 <149188763+battleooze244@users.noreply.github.com>
2023-10-28 02:02:07 +0530

    Update workaround.py

--- workaround.py
+++ workaround.py
@@ -13,10 +13,10 @@ def update_executable_path():
   #   for line in file:
   #     if "chromedriver_path" in line:
   #       # line = line.replace('"hi"', f'"{output}"')
-  #       chromedriver_path = output  # Assign the updated path to chromedriver_path
-  #       line = line.replace('update_executable_path()',
-  #                           f'"{chromedriver_path}"')
-  #     print(line, end='')
-  #     # if "update_executable_path():" in line:
-  #     #   remove = True
-  # print("Executable path updated successfully.")
+        chromedriver_path = output  # Assign the updated path to chromedriver_path
+        line = line.replace('update_executable_path()',
+                            f'"{chromedriver_path}"')
+      print(line, end='')
+      # if "update_executable_path():" in line:
+      #   remove = True
+  print("Executable path updated successfully.")
